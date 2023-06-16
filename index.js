a44fdea647cd387276f686c7b8e6d69ddc056d65
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-06-17 03:11:34 +0530

    Created Hamburger Menu for smaller devices

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const ham = document.querySelector('.fa-bars');
 const navbar = document.getElementById('navbar');
+const navlinks = document.querySelectorAll('#navbar a');
 ham.addEventListener('click', () => {
     navbar.classList.toggle('active');
 })
+navlinks.forEach(navlink => {
+    navlink.addEventListener('click', () => {
+        navbar.classList.remove('active');
+    })
+});
