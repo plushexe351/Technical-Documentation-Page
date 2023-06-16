8d0ef274b7304850a99aed7e1bbcf1b966ab2724
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-06-17 04:42:21 +0530

    fixed bug

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const ham = document.querySelector('.fa-bars');
 const navbar = document.getElementById('navbar');
 const navlinks = document.querySelectorAll('#navbar a');
+const sections = document.querySelectorAll('section');
+
+sections.forEach(section => {
+    section.addEventListener('click', () => {
+        if (navbar.classList.contains('active')) {
+            navbar.classList.remove('active');
+        }
+    })
+})
+
 ham.addEventListener('click', () => {
     navbar.classList.toggle('active');
 })
@@ -8,10 +18,4 @@ navlinks.forEach(navlink => {
     navlink.addEventListener('click', () => {
         navbar.classList.remove('active');
     })
-});
-
-navbar.addEventListener('click', () => {
-    if (navbar.classList.contains('active')) {
-        navbar.classList.remove('active');
-    }
 });
